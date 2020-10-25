6e4c0cc19afe64de22f4fa14e02654c3e4980a87
gilad <gilad2.m@gmail.com>
2020-10-25 10:02:25 +0200

    Initial Commit

---
+++ BankDemo/BankDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankDemoApp.swift
+//  BankDemo
+//
+//  Created by gilad on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BankDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
